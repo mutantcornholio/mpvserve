b79de8d29ccc4356a169ce7d8eaec8c614220e6d
cornholio <0@mcornholio.ru>
2022-11-16 19:14:11 +0100

    Fixing nested paths

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 use std::path::Path;
 
 pub fn get_urlencoded_path(path_from_root: &Path) -> Option<String> {
-    let mut res = String::from("/");
+    let mut res = String::from("");
 
     for path_item in path_from_root.iter() {
         let item = path_item.to_str()?;
+        res += "/";
         res += &String::from(urlencoding::encode(item));
     }
 
